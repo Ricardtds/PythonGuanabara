1388a415802777585faa23686fda36208a395e39
Ricardo Teixeira <ricardoteixeirasantos@hotmail.com>
2021-11-12 12:10:45 -0300

    Estudo aula 16

---
+++ exercicios/ex072.py
@@ -0,0 +1,3 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até 20.
+
+# Seu programa deverá ler um número pelo telcado (entre 0 e 20) e mostrá-lo por extenso.
---
+++ exercicios/ex073.py
@@ -0,0 +1,10 @@
+# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem
+# de colocação. Depois mostre:
+
+# Apenas os 5 primeiros colocados.
+
+# Os últimos 4 colocados tabela.
+
+# Uma lista com os times em ordem alfabética.
+
+# Em que posição na tabela está o time da chapecoense.
